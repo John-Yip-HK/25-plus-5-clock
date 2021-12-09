2eea0f407c747999238eefb814ff0661d0e67161
John-Yip-HK <yip4work@gmail.com>
2021-12-09 17:05:08 +0800

    Initialised project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col><h3>25 + 5 Clock</h3></Col>
+      </Row>
+      <Row>
+        <Container>
+          <Row>
+            <Col>Session</Col>
+          </Row>
+          <Row>
+            <Col><h2>25:00</h2></Col>
+          </Row>
+          <Row>
+            <Col>Break: 5:00</Col>
+          </Row>
+        </Container>
+      </Row>
+      <Row>
+        <Button>Start</Button>
+      </Row>
+      <Row>
+        <Button>Settings</Button>
+      </Row>
+      <Row>
+        <Button>Reset Timer</Button>
+      </Row>
+    </Container>
   );
 }
 
